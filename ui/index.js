62cb8ecdbd322891a71ebfc32f68eec708a5a87b
Victor Matekole <victor@matekole.com>
2024-02-25 21:45:05 +0100

    feature: start of ui

---
+++ ui/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/static', express.static('static'));
